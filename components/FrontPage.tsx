70cb15efe4b5472adf500e775c573406f349138b
Mike <mikesmallhelp@gmail.com>
2023-12-18 18:33:31 +0200

    Test application created

---
+++ components/FrontPage.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import PageTemplate from './PageTemplate';
+
+const FrontPage = ({ pageDirectory, showLogoutLink = false }: { pageDirectory: string, showLogoutLink?: boolean }) => {
+    const [loading, setLoading] = useState(true);
+
+    const content = (
+        <div>
+            <a href='/working-page'>Link for a working page</a>
+            <a href={`/${pageDirectory}`}>{`Link for a ${pageDirectory} page`}</a>
+        </div>
+    );
+
+    return (
+        <>
+            <PageTemplate title={'wacat test application'} content={content} 
+                   loading={loading} setLoading={setLoading} showLogoutLink={showLogoutLink}/>
+        </>
+    );
+};
+
+export default FrontPage;

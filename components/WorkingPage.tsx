a69535bd5eb76b3f51e6fd076c26ebe4b35c1b57
Mike <mikesmallhelp@gmail.com>
2024-03-19 16:39:49 +0200

    Edit the test application

--- components/WorkingPage.tsx
+++ components/WorkingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import PageTemplate from './PageTemplate';
 import { fetchData } from '../util/util';
 
@@ -17,16 +17,51 @@ const WorkingPage = ({ title }: { title: string }) => {
 
     const PageContent = (
         <div>
+            <div>Name:</div>
             <input />
+            <br />
 
+            <div>Menu selection:</div>
             <select name="someValues" id="someValues">
-                <option value="aaa">aaa</option>
-                <option value="bbb">bbb</option>
+                <option value="Fish">Fish</option>
+                <option value="Meat">Meat</option>
             </select>
+            <br />
 
+            <div>Eat a desert:</div>
             <input type="checkbox" />
+            <br />
 
-            <input type="date"/>
+            <div>Age:</div>
+            <div>
+                <input type="radio" id="age1" name="age" value="age1" />
+                <label htmlFor="age1">under 30</label>
+            </div>
+            <div>
+                <input type="radio" id="age2" name="age" value="age2" />
+                <label htmlFor="age2">30-45</label>
+            </div>
+            <div>
+                <input type="radio" id="age3" name="age" value="age3" />
+                <label htmlFor="age3">over 45</label>
+            </div>
+            <br />
+
+            <div>Registration email:</div>
+            <input type="email" />
+            <br />
+
+            <div>Registration password:</div>
+            <input type="password" />
+            <br />
+
+            <div>Search for help:</div>
+            <input type="search" />
+            <br />
+
+            <div>Your homepage url:</div>
+            <input type="url" />
+            <br />
 
             <button onClick={handleButtonClick}>Normal button</button>
 
